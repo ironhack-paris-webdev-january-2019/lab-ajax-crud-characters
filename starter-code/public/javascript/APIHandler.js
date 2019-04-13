da32fff5687dc5633d5a97d72d1f95b13bc31734
ta-webpt-par <eloji9@hotmail.com>
2019-04-13 11:13:19 +0200

    Solution

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,82 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.myaxios = axios.create({
+      baseURL: this.BASE_URL
+    });
   }
 
-  getFullList () {
-
+  getFullList() {
+    this.myaxios.get(`/characters`).then(res =>
+      res.data.forEach(character => {
+        createCard(
+          character.name,
+          character.occupation,
+          character.weapon,
+          character.cartoon
+        );
+      })
+    );
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    this.myaxios.get(`/characters/${id}`).then(res => {
+      createCard(
+        res.data.name,
+        res.data.occupation,
+        res.data.weapon,
+        res.data.cartoon
+      );
+    });
   }
 
-  createOneRegister () {
+  createOneRegister(name, occupation, weapon, cartoon) {
+    let newCharacter = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon
+    };
 
+    this.myaxios.post(`/characters`, newCharacter)
+      .then(() => {
+        $("#send-data").css("background-color", "green");
+        this.getFullList();
+      })
+      .catch(err => {
+        $("#send-data").css("background-color", "red");
+        console.log(err);
+      });
   }
 
-  updateOneRegister () {
+  updateOneRegister(id, name, occupation, weapon, cartoon) {
+    let character = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon
+    };
 
+    this.myaxios.patch(`/characters/${id}`, character)
+      .then(() => {
+        $("#send-data").last().css("background-color", "green");
+        this.getFullList();
+      })
+      .catch(err => {
+        $("#send-data").last().css("background-color", "red");
+        console.log(err);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    this.myaxios
+      .delete(`/characters/${id}`)
+      .then(() => {
+        $("#delete-one").css("background-color", "green");
+        this.getFullList();
+      })
+      .catch(() => {
+        $("#delete-one").css("background-color", "red");
+      });
   }
 }
